5c63079d73a52457d9a43956324bac68ae26972a
unknown <nickewideving@gmail.com>
2023-12-02 19:31:02 +0100

    day2

--- day2.js
+++ day2.js
@@ -28,7 +28,7 @@ const resultPart1 = parsedGames.reduce((acc, game, index) => {
 
 console.log(resultPart1);
 
-const resultPart2 = parsedGames.reduce((totalSum, game) => {
+const resultPart2 = parsedGames.reduce((totalPower, game) => {
 	const maxValues = game.reduce((max, subset) => {
 		const colors = Object.keys(subset)
 		for (const color of colors) {
@@ -37,7 +37,7 @@ const resultPart2 = parsedGames.reduce((totalSum, game) => {
 		return max
 	}, { ...Object.fromEntries(Object.keys(bag).map(key => [key, 0]))});
 	const power = Object.values(maxValues).reduce((total, value) => total * value, 1);
-	return totalSum + power;
+	return totalPower + power;
 }, 0);
 
 console.log(resultPart2);
