181328392a276e084cf0fec36bd29ee786ebe64a
unknown <nickewideving@gmail.com>
2023-12-09 01:19:39 +0100

    day8

---
+++ day8.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('day8-input.txt', 'utf8').split`\r\n`
+
+const instructions = input[0].split``.map(v => v === 'R' ? 1 : 0)
+
+const parsedData = input.slice(2).reduce((acc, curr) => {
+	const [key, values] = curr.split` = `
+	const parsedValues = values.replace(/[() ]/g, '').split`,`;
+	acc[key] = parsedValues
+	return acc
+}, {});
+
+let resultPart1 = 0;
+let instructionIndex = 0;
+let indexOfNode = Object.keys(parsedData).indexOf('AAA');
+let currentNode;
+const entries = Object.entries(parsedData);
+const nodes = Object.keys(parsedData);
+while (currentNode !== 'ZZZ') {
+	const nodeValues = entries[indexOfNode][1];
+	currentNode = nodeValues[instructions[instructionIndex]];
+	indexOfNode = nodes.indexOf(currentNode);
+	instructionIndex = (instructionIndex + 1) % instructions.length
+	resultPart1++;
+}
+
+console.log(resultPart1);
