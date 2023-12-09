e7f6c8f3f37e30cc9403e3437360e15828bdc4c3
unknown <nickewideving@gmail.com>
2023-12-09 01:48:21 +0100

    day8

--- day8.js
+++ day8.js
@@ -13,17 +13,18 @@ const parsedData = input.slice(2).reduce((acc, curr) => {
 
 let resultPart1 = 0;
 let instructionIndex = 0;
-let indexOfNode = Object.keys(parsedData).indexOf('AAA');
+let nodeIndex = Object.keys(parsedData).indexOf('AAA');
 let currentNode;
 
 const targetNode = 'ZZZ'
-const entries = Object.entries(parsedData);
+const network = Object.entries(parsedData);
 const nodes = Object.keys(parsedData);
 
 while (currentNode !== targetNode) {
-	const nodeValues = entries[indexOfNode][1];
-	currentNode = nodeValues[instructions[instructionIndex]];
-	indexOfNode = nodes.indexOf(currentNode);
+	const [_, nodeValues] = network[nodeIndex];
+	const instruction = instructions[instructionIndex]
+	currentNode = nodeValues[instruction];
+	nodeIndex = nodes.indexOf(currentNode);
 	instructionIndex = (instructionIndex + 1) % instructions.length
 	resultPart1++;
 }
